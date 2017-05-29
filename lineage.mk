2d1117d67a578c19fd4c83cc7d9c1c818131498c
Brinly Taylor <brinly@brinly.me>
2017-05-29 06:57:07 +0000

    tenshi: Set correct device name and fingerprint.
    * Google Play Store isn't officially supported by Lineage, but this should fix issues
    where users had the potential to not see certain apps in Google Play (WhatsApp etc)
    
    Change-Id: Ifd608d6638ebca24f9afcde3eeaec82dfb6a4e61

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,6 +30,10 @@ PRODUCT_NAME := lineage_chaozu
 PRODUCT_BRAND := bq
 PRODUCT_MODEL := Aquaris U
 PRODUCT_MANUFACTURER := bq
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=chaozu
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
