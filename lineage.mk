LineageOS/android_device_bq_tenshi

tenshi: rebrand from CM to LineageOS
* We actually started building for LineageOS first but always used the CM "branding".

Change-Id: I2cd8172359ddeeea0d0efa5bd801fab2deea9997

lineage.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := bq
 PRODUCT_DEVICE := tenshi
-PRODUCT_NAME := cm_tenshi
+PRODUCT_NAME := lineage_tenshi
 PRODUCT_BRAND := bq
 PRODUCT_MODEL := Aquaris U Plus
 PRODUCT_MANUFACTURER := bq

Tigrouzen/android_device_bq_chaozu

lineage.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := bq
 PRODUCT_DEVICE := chaozu
-PRODUCT_NAME := cm_chaozu
+PRODUCT_NAME := lineage_chaozu
 PRODUCT_BRAND := bq
 PRODUCT_MODEL := Aquaris U
 PRODUCT_MANUFACTURER := bq