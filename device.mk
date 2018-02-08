LineageOS/android_device_bq_tenshi

Revert "tenshi: Always include vendor makefiles"
 * This will break weeklies.

This reverts commit 00d6042abfda830cade171ec52ad9048b5d9b9c7.

Change-Id: Ia6c1b6945543649c327bb9e46d34950f4da220b3

device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/bq/tenshi/tenshi-vendor.mk)
+$(call inherit-product-if-exists, vendor/bq/tenshi/tenshi-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay

Tigrouzen/android_device_bq_chaozu

device.mk
@@ -20,7 +20,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvi
 
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/bq/chaozu/chaozu-vendor.mk)
+$(call inherit-product-if-exists, vendor/bq/chaozu/chaozu-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay