LineageOS/android_device_bq_tenshi

tenshi: Always include vendor makefiles
Change-Id: I78370516dc62b016e710fba991c009680ee2c7df

device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/bq/tenshi/tenshi-vendor.mk)
+$(call inherit-product, vendor/bq/tenshi/tenshi-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay

Tigrouzen/android_device_bq_chaozu

device.mk
@@ -20,7 +20,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvi
 
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/bq/chaozu/chaozu-vendor.mk)
+$(call inherit-product, vendor/bq/chaozu/chaozu-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay