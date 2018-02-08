LineageOS/android_device_bq_tenshi

tenshi: Use lineage_tenshi defconfig
Change-Id: I04f9603bed9282117344bb4c42130cbebd20fea3

BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 -
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-TARGET_KERNEL_CONFIG := lineageos_tenshi_defconfig
+TARGET_KERNEL_CONFIG := lineage_tenshi_defconfig
 TARGET_KERNEL_SOURCE := kernel/bq/msm8937
 
 # Audio

Tigrouzen/android_device_bq_chaozu

BoardConfig.mk
@@ -45,7 +45,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_APPEND_DTB := true
-TARGET_KERNEL_CONFIG := lineageos_chaozu_defconfig
+TARGET_KERNEL_CONFIG := lineage_chaozu_defconfig
 TARGET_KERNEL_SOURCE := kernel/bq/msm8937
 
 # Audio
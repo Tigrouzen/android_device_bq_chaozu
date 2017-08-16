e72e8457f6e71a81701e8c8eeacf966d8a2d945d
Eloi Carracedo <eloimuns@gmail.com>
2017-08-16 15:16:51 +0200

    tenshi: Use lineage_tenshi defconfig
    Change-Id: I04f9603bed9282117344bb4c42130cbebd20fea3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_APPEND_DTB := true
-TARGET_KERNEL_CONFIG := lineageos_chaozu_defconfig
+TARGET_KERNEL_CONFIG := lineage_chaozu_defconfig
 TARGET_KERNEL_SOURCE := kernel/bq/msm8937
 
 # Audio
