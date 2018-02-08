LineageOS/android_device_bq_tenshi

tenshi: move local path to top.
We need to include this route before first use

Change-Id: Idef33abe8e24a29562083a901861f699dbfc329e

BoardConfig.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/bq/tenshi
+
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Aquaris_U_Plus,tenshi
-
-LOCAL_PATH := device/bq/tenshi
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8937

Tigrouzen/android_device_bq_chaozu

BoardConfig.mk
@@ -14,12 +14,12 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/bq/chaozu
+
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Aquaris_U,chaozu
-
-LOCAL_PATH := device/bq/chaozu
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8937